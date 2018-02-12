764941eda1a16c3e7208e78072e3b90a981ed18f
Domenico <dtropeano@hotmail.it>
2018-02-12 15:02:32 +0100

    old test init

--- src/test/Old_test.java
+++ src/test/Old_test.java
@@ -1,21 +1,24 @@
-
-
-
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 /**
- * @author Domenico Antonio Tropeano on 12/02/2018 at 14:16
+ * @author Domenico Antonio Tropeano on 12/02/2018 at 14:56
  * @project MobileSolutions
  */
-public class TestSelenium {
-    public static void main(String[] args) {
+public class Old_test {
+    private static ChromeDriver driver;
+    @Before
+    public void setUp(){
         System.setProperty("webdriver.chrome.driver", "D:\\Documenti\\chromedriver_win32\\chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.get("http://localhost:8080");
+    }
+    @Test
+    public void testLogin(){
         WebElement usernameStuff = driver.findElement(By.name("username"));
         usernameStuff.sendKeys("umber");
         WebElement passwordStuff = driver.findElement(By.name("password"));
